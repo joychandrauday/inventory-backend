8f47c89326c4a9969634711007d00c8a3d2782b3
Joy Chandra Uday <joychandraud@gmail.com>
2025-06-29 14:04:32 +0600

    updated

--- app.ts
+++ app.ts
@@ -2,15 +2,40 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import { categoryRoutes } from './routes/category.routes';
 import { productRoutes } from './routes/product.routes';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 // Middleware
 app.use(cors({
-    origin: ['http://localhost:5137', 'https://inventorymanagement-hazel.vercel.app'],
+    origin: [],
     credentials: true
 }));
+// Allowed origins
+const allowedOrigins = [
+    'http://localhost:5137', 'https://inventorymanagement-hazel.vercel.app'
+];
+
+// CORS middleware
+app.use(
+    cors({
+        origin: (origin, callback) => {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'), false);
+            }
+        },
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+        allowedHeaders: ['Content-Type', 'Authorization'],
+        credentials: true,
+    })
+);
+
+app.options('*', cors());
+
 app.use(express.json());
+app.use(bodyParser.json());
 
 // Routes
 app.get('/', (req: Request, res: Response) => {
